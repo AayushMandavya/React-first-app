8113ed6a3a49f229b176b7740712fee7e206f7f2
aayushmandavya <mandavyaaayush98@gmail.com>
2023-04-12 10:41:02 +0545

    first commit with fixes

--- src/App.js
+++ src/App.js
@@ -22,22 +22,22 @@ function App() {
            <td>32132131231</td>
   </tr>
 
-  <tr> <td>1</td>
-         <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-           <td>32132131231</td>
+  <tr> <td>2</td>
+         <td>Bijay Futterkiste</td>
+          <td>Maria Manders</td>
+           <td>6455646565</td>
   </tr>
   <tr>
-          <td>1</td>
-         <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-           <td>32132131231</td>
+          <td>3</td>
+         <td>Rojan Futterkiste</td>
+          <td>Maria Sanders</td>
+           <td>7987455321</td>
   </tr>
   <tr>
-          <td>1</td>
-         <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-           <td>32132131231</td>
+          <td>4</td>
+         <td>Rohan Futterkiste</td>
+          <td>Maria Ganders</td>
+           <td>4664464644</td>
   </tr>
 
       </table>
